aedf0aa7de37272aa2f171d2ea33f6b211acc478
Lucky <luckymittapelly44@gmail.com>
2022-11-15 20:37:36 +0530

    l4

---
+++ __tests__/todo.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+
+const todoList = require("../todos");
+const { all, markAsComplete, add } = todoList();
+// eslint-disable-next-line no-undef
+describe("Todo Test suite", () => {
+  // eslint-disable-next-line no-undef
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
+  // eslint-disable-next-line no-undef
+  test("should add new todo", () => {
+    const todoItemCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    // eslint-disable-next-line no-undef
+    expect(all.length).toBe(todoItemCount + 1);
+  });
+  // eslint-disable-next-line no-undef
+  test("should mark a todo as completed", () => {
+    // eslint-disable-next-line no-undef
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+
+    expect(all[0].completed).toBe(true);
+  });
+});
